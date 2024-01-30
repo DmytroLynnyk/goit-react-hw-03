439c19341ecc83243020aeb3585a60d51aa16e02
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-01-30 14:01:38 +0200

    Task 02 work in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import './App.css';
 import { ContactList } from './components/contactlist/contactlist';
+import { SearchBox } from './components/searchbox/searchbox';
 
 const users = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -9,12 +11,37 @@ const users = [
 ];
 
 export const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [filterUsers, setFilterUsers] = useState(users);
+
+  // const handleChange = evt => {
+  //   setInputValue(evt.target.value);
+  // };
+
+  // console.log(inputValue);
+
+  const visibleUsers = filterUsers.filter(filterUser =>
+    filterUser.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
+
       {/* <ContactForm /> */}
-      {/* <SearchBox /> */}
-      <ContactList users={users} />
+
+      {/* <SearchBox value={inputValue} onChange={setInputValue} /> */}
+
+      <div>
+        <p>Find contacts by name</p>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={evt => setInputValue(evt.target.value)}
+        />
+      </div>
+
+      <ContactList users={visibleUsers} />
     </div>
   );
 };
---
+++ src/components/searchbox/searchbox.jsx
@@ -0,0 +1,12 @@
+export const SearchBox = (value, onChange) => {
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        value={value}
+        onChange={evt => onChange(evt.target.value)}
+      />
+    </div>
+  );
+};
